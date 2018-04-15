42701a942fae3443d047d27103b1cbdec695a6c1
Carolyn Smith <casmith205@gmail.com>
2018-04-15 18:59:43 -0500

    fixed deployment connection

--- config/connection.js
+++ config/connection.js
@@ -1,8 +1,12 @@
 // Require mysql 
 var mysql = require("mysql");
+var connection;
 
 // Create the connection
-var connection = mysql.createConnection({
+if (process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+  } else {
+    connection = mysql.createConnection({
     host: "localhost",
     user: "root",
     password: "root",
