5828bd0da004242ed0e4bfaa0be30355f2af1ab2
Carolyn Smith <casmith205@gmail.com>
2018-04-15 19:24:13 -0500

    updating for using jaws mysql

--- db/schema.sql
+++ db/schema.sql
@@ -5,9 +5,10 @@ USE burgers_db;
 
 -- CREATE THE BURGERS TABLE
 CREATE TABLE burgers (
-    id INT AUTO_INCREMENT,
+    id INT AUTO_INCREMENT NOT NULL,
     burger_name VARCHAR(255),
     devoured BOOLEAN,
+    createdAt TIMESTAMP NOT NULL,
     PRIMARY KEY (id)
 );
 
